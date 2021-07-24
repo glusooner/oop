7b4380231854d03093cd0ca0af97e406a99b17fb
glusooner <54253630+glusooner@users.noreply.github.com>
2021-07-23 21:16:26 -0500

    learning oop 2nd commit

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-class Customer:
+'fun begins here'
+class User:
+    def log(self):
+        print('i am a customer')
+
+class Teacher(User):
+    def log(self):
+        print('i am a teacher')
+
+class Customer(User):
     def __init__(self,name,membership_type):
         self.name = name
         self.membership_type = membership_type
-        print('customer created')
+        
+    def __str__(self):
+       return self.name + ":" + self.membership_type
+
+    def __eq__(self,other):
+        if self.name == other.name:
+            return True
+        return False
+
+    def upgrade(self,new_membership):
+        self.membership_type = new_membership
+
+    def print_all(customers):
+        for customer in customers:
+            print(customer)
 
 
-c = Customer('caleb','gold')
-print(c.name, c.membership_type)
+    __repr__=__str__
+
+users =[Customer('caleb','gold'),
+        Customer('george','platinum'),
+        Teacher()]
+
+
+
+
+for user in users:
+    user.log()
